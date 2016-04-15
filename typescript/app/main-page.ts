676ce427f803332e9ec2dd58721d6c2b54e06639
Todd Anglin <toddanglin@gmail.com>
2016-04-15 18:12:10 -0500

    Added animation example

--- typescript/app/main-page.ts
+++ typescript/app/main-page.ts
@@ -1,12 +1,21 @@
 import MainViewModel from './main-view-model';
 import { EventData } from 'data/observable';
 import { Page } from 'ui/page';
+import { AnimationCurve } from 'ui/enums';
 
 var viewModel = new MainViewModel();
 
 let onNavigatingTo = (args: EventData) => {
     var page = <Page>args.object;
     page.bindingContext = viewModel;
+    
+    var lbl = page.getViewById("todoTitle");
+    lbl.animate({
+        translate: { x: 0, y: 150},
+        duration: 400,
+        delay: 800,
+        curve: AnimationCurve.spring
+    });
 }
 
 export { onNavigatingTo }
