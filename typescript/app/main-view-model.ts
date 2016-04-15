1266db15bbfed3681580915772c608126583ad76
Todd Anglin <toddanglin@gmail.com>
2016-04-15 18:12:48 -0500

    Added navigation example with about page

--- typescript/app/main-view-model.ts
+++ typescript/app/main-view-model.ts
@@ -1,35 +1,51 @@
 import { Observable } from 'data/observable';
 import { ObservableArray } from 'data/observable-array';
+import { topmost } from 'ui/frame';
 
 class MainViewModel extends Observable {
-    newTodo: string = "";
-    todos: ObservableArray<string>;
-    
-    constructor() {
-        super();
-        
-        this.todos = new ObservableArray([]);
-        
-        
+  newTodo: string = "";
+  todos: ObservableArray<string>;
+
+  constructor() {
+    super();
+
+    this.todos = new ObservableArray([]);
+
+
+  }
+
+  navigate() {
+    var navigationEntry = {
+      moduleName: "about",
+      animated: true,
+      transition: {
+        name: "fade",
+        duration: 380,
+        curve: "easeIn"
+      }
+    };
+    topmost().navigate(navigationEntry);
+
+    //topmost().navigate("about");
+  };
+
+  add(args) {
+    if (this.get("newTodo").trim().length > 0) {
+      this.todos.push(this.get("newTodo").trim());
+
+      //Clear the input field observable
+      this.set("newTodo", "");
     }
-    
-    add(args) {
-      if (this.get("newTodo").trim().length > 0) {
-        this.todos.push(this.get("newTodo").trim());
-        
-        //Clear the input field observable
-        this.set("newTodo", "");           
-      } 
-    };
-    
-    total = function() {
-      return this.todos.length;
-    };
-    
-    clearCompleted(args) {
-      alert("TODO: Clear completed todos");
-      console.log("Clear Completed", args);
-    };
+  };
+
+  total = function () {
+    return this.todos.length;
+  };
+
+  clearCompleted(args) {
+    alert("TODO: Clear completed todos");
+    console.log("Clear Completed", args);
+  };
 }
 
 export default MainViewModel;
